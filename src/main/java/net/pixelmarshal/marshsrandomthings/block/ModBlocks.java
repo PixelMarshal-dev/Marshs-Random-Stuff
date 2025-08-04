e37dec52121064fd3a1c76c97e502508f6eb2f75
PixelMarshal <jmeyer3225@gmail.com>
2025-08-04 13:46:37 -0400

    Adding in Block Support

--- src/main/java/net/pixelmarshal/marshsrandomthings/block/ModBlocks.java
+++ src/main/java/net/pixelmarshal/marshsrandomthings/block/ModBlocks.java
@@ -3,24 +3,27 @@ package net.pixelmarshal.marshsrandomthings.block;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.pixelmarshal.marshsrandomthings.MarshThings;
 import net.pixelmarshal.marshsrandomthings.item.ModItems;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MarshThings.MOD_ID);
 
-    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
-        DeferredBlock<T> toReturn = BLOCKS.register(name, block);
-        registerBlockItem(name, toReturn);
+    private static <B extends Block> DeferredBlock<B> registerBlock(String name, Function<BlockBehaviour.Properties, ? extends B> blockFactory,
+                                                                    BlockBehaviour.Properties blockProperties) {
+        DeferredBlock<B> block = BLOCKS.registerBlock(name, blockFactory, blockProperties);
+        registerBlockItem(name, block);
+        return block;
     }
 
-    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    private static <B extends Block> void registerBlockItem(String name, DeferredBlock<B> block) {
+        ModItems.ITEMS.registerSimpleBlockItem(name,block);
     }
 
     public static void register(IEventBus eventBus) {
